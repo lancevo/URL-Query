2d0fa2a85aed3efd0f51987ed2b78f9bbea84d4d
Lance Vo <lvo@vinavo.us>
2012-10-16 14:13:10 -0500

    fix empty property because query string start with &, shorten object to window.query
    for example: ?&prop=val

--- urlquery.js
+++ urlquery.js
@@ -9,27 +9,27 @@ Example
 URL:	http://example.com/page/?abc=123&xyz=456
 
 Get Queries:
-	alert(window.location.query.abc); // print value: 123
-	alert(window.location.query.xyz); // print value: 456
+	alert(window.query.abc); // print value: 123
+	alert(query["abc"]); // print value 123
+	alert(window.query.xyz); // print value: 456
 		
 */
 
 (function(){
-	if (window.location.query) {
-		return;
-	}
 	var queries = window.location.search.substr(1).split("&"),
-		params = {};
+		params = {},
+		valPos;
 
 	for (var i=0,j=queries.length; i<j; i++) {
-	    var valPos = queries[i].indexOf("="),
-			tmp;
+		if (!queries[i]) {
+			continue;
+		}
+	    valPos = queries[i].indexOf("=")
 		if (valPos === -1) {
 			params[queries[i]] = "";
 		} else {
-			params[queries[i].substring(0, valPos)] = queries[i].substring(valPos+1) ? queries[i].substring(valPos+1) : "";			
+			params[queries[i].substring(0, valPos)] = queries[i].substring(valPos+1) ? queries[i].substring(valPos+1) : "";
 		}
-		
 	}
-	window.location.query = params;
-}())
+	window.query = params;
+})();
